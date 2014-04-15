d53d831a4b50debfe9c2bc428c7e31b483d428e8
amadeuszg22 <amadeusz.grabowski@gmail.com>
2014-04-15 15:04:57 +0000

    bootstrap changed

--- LB/bootstrap.sh
+++ LB/bootstrap.sh
@@ -9,5 +9,6 @@ echo "System install git & clone system configurations"
 sudo apt-get install git -y
 sudo mkdir /sysrepo
 sudo git clone git@github.com:amadeuszg22/iamg.git /sysrepo
- 
+echo "System copy keys"
+sudo rsync -artvpolz /sysrepo/iamg/LB/config/.ssh/ /root/.ssh/
 
