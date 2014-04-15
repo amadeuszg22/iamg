6e589fc35dd9ca7db2945724493e4a8cb1c43022
amadeuszg22 <amadeusz.grabowski@gmail.com>
2014-04-15 14:58:46 +0000

    key config created

--- LB/bootstrap.sh
+++ LB/bootstrap.sh
@@ -4,3 +4,10 @@ echo "System will install server features"
 
 
 echo "System Upgrades repositories"
+sudo apt-get update
+echo "System install git & clone system configurations"
+sudo apt-get install git -y
+sudo mkdir /sysrepo
+sudo git clone git@github.com:amadeuszg22/iamg.git /sysrepo
+ 
+
